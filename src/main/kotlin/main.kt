d42ac8c583a097fdde4793f1817f20378ed58b61
ChecoChecoGH <as.ignasi97@gmail.com>
2021-04-26 13:18:46 +0200

    Terminado, falta matizar si hay algo mal hecho

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,8 @@
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-val TAMCLASE = 30-1
+const val TAMCLASE = 30-1
 
 fun main() {
     /*Vamos a imaginar el siguiente contexto, tenemos un profesor con 30 alumnos.
@@ -18,18 +17,33 @@ fun main() {
     val listaNombres = mutableListOf("Pepe", "Juan", "Pablo", "Paula", "Elena", "Paloma")
     var auxListaNombres = 0
     val listaAlumnos = mutableListOf<Alumno>()
+    val examen = Examen("")
     for(i in 0..TAMCLASE){
         if (auxListaNombres >= 6)
             auxListaNombres = 0
-        val alumno = Alumno(listaNombres[auxListaNombres++]+(0..100).random())
+        val alumno = Alumno(listaNombres[auxListaNombres++]+(0..100).random(), examen)
         listaAlumnos.add(alumno)
     }
+
     val listaExamenes = mutableListOf<Examen>()
-    val examen = Examen()
     for(i in 0..TAMCLASE){ listaExamenes.add(examen) }
 
-    val profesor = Profesor("Carlos",listaExamenes)
+    /*val matrizAlumnoYExamen = mutableListOf(listaAlumnos, listaExamenes)
 
+    matrizAlumnoYExamen.forEach {
+        it.forEach {
+            if (it is Alumno){
+                println(it.nombre)
+            }else if(it is Examen){
+                println(it.rellenarExamen())
+            }else
+                println("Tipo de dato incorrecto")
+        }
+    }
+    */
+    val profesor = Profesor("Carlos", listaAlumnos, listaExamenes)
+
+    //Llegada de alumnos a clase
     runBlocking {
         listaAlumnos.forEach {
             this.launch {
@@ -38,30 +52,46 @@ fun main() {
             }
         }
     }
+
     println(profesor.nombre + profesor.repartirExamen())
 
     runBlocking {
-        listaAlumnos.forEach {
+        listaAlumnos.forEach { alumno ->
             this.launch {
-
+                delay(alumno.examen.rellenarExamen()*1000L)
+                println(alumno.acabadoExamen())
             }
         }
-
     }
 
+    println("Hemos acabado el examen, podemos comenzar con la desinfección")
+
+
+
+
+
+
 
 
 
 
 }
 
-class Profesor(val nombre : String, val listaExamenes : MutableList<Examen>){
-    fun repartirExamen() : String{ return " reparte exámenes"}
+class Profesor(val nombre: String, val listaAlumnos : MutableList<Alumno>, val listaExamen: MutableList<Examen>){
+    fun repartirExamen(): String{
+        listaAlumnos.forEach { alumno ->
+            listaExamen.forEach { examen ->
+                examen.nombre = alumno.nombre
+            }
+        }
+        return "ha repartido el examen a cada alumno"
+    }
+
 }
 class Alumno(val nombre : String, val examen : Examen){
-    fun llegar() : String{ return " ha llegado"}
-    fun hacerExamen() : String{ return " hace el examen"}
+    fun llegar() : String{ return " he llegado"}
+    fun acabadoExamen() : String{ return "El alumno $nombre ha terminado el examen"}
 }
-class Examen(){
-    fun rellenarExamen() : Int{ return (1..4).random()}
+class Examen(var nombre : String) {
+    fun rellenarExamen() : Int{ return ((1..4).random())}
 }
