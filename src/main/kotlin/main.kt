2f76d21a1b1383feaeb2923182f3cb9b5406d350
ChecoChecoGH <as.ignasi97@gmail.com>
2021-04-29 14:10:05 +0200

    terminado y comentado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,46 +1,35 @@
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.random.Random
 
 const val TAMCLASE = 30-1
 
 fun main() {
-    /*Vamos a imaginar el siguiente contexto, tenemos un profesor con 30 alumnos.
-    Los alumnos son de una clase Alumno. Los alumnos tienen 2 métodos, decir "he llegado" y "hacer el examen".
-    Cada alumno tarda de 1 a 6 segundo en llegar (Número aleatorio). Cuando llegan dicen "Alumno X ha llegado".
-    Cuando todos han llegado, el profesor reparte los examenes.
-    Los examenes son otra clase que tiene un método que se llama hacer y que lleva un número aleatorio de 1 a 4 en completarse.
-    Cuando el alumno ha completado el examen, dice "El Alumno X ha terminado el examen".
-    Cuando todos los alumnos han terminado, el profesor recoge los exámenes y da pro concluido el examen.
-    */
-
+    //Listas
     val listaNombres = mutableListOf("Pepe", "Juan", "Pablo", "Paula", "Elena", "Paloma")
-    var auxListaNombres = 0
     val listaAlumnos = mutableListOf<Alumno>()
-    val examen = Examen("")
+    val listaExamenes = mutableListOf<Examen>()
+
+    //aux y tamaño de listaNombres
+    var auxListaNombres = 0
+    val tamListaNombres = listaNombres.size
+
+    //relleno la lista de examenes con examenes vacíos
     for(i in 0..TAMCLASE){
-        if (auxListaNombres >= 6)
+        val examen = Examen("", 0.0)
+        listaExamenes.add(examen)
+    }
+
+    //relleno la listaAlumnos, cada alumno con su nombre y su examen
+    for(i in 0..TAMCLASE){
+        if(auxListaNombres >= tamListaNombres)
             auxListaNombres = 0
-        val alumno = Alumno(listaNombres[auxListaNombres++]+(0..100).random(), examen)
+        val alumno = Alumno(listaNombres[auxListaNombres++]+(i+1), listaExamenes[i])
         listaAlumnos.add(alumno)
     }
 
-    val listaExamenes = mutableListOf<Examen>()
-    for(i in 0..TAMCLASE){ listaExamenes.add(examen) }
-
-    /*val matrizAlumnoYExamen = mutableListOf(listaAlumnos, listaExamenes)
-
-    matrizAlumnoYExamen.forEach {
-        it.forEach {
-            if (it is Alumno){
-                println(it.nombre)
-            }else if(it is Examen){
-                println(it.rellenarExamen())
-            }else
-                println("Tipo de dato incorrecto")
-        }
-    }
-    */
+    //creo el profesor con su nombre, la listaAlumnos y la listaExamenes
     val profesor = Profesor("Carlos", listaAlumnos, listaExamenes)
 
     //Llegada de alumnos a clase
@@ -53,8 +42,10 @@ fun main() {
         }
     }
 
-    println(profesor.nombre + profesor.repartirExamen())
+    //Como ya estan todos los alumnos el profesor reparte los examenes
+    println(profesor.repartirExamen())
 
+    //los examenes se rellenan y cada alumno dice cuando ha terminado el examen
     runBlocking {
         listaAlumnos.forEach { alumno ->
             this.launch {
@@ -64,34 +55,32 @@ fun main() {
         }
     }
 
-    println("Hemos acabado el examen, podemos comenzar con la desinfección")
-
-
-
-
-
-
-
-
-
-
+    //los examenes están todos entregados asique el profesor pone las notas
+    println("\n${profesor.nombre} ya tiene todos los examenes y los corrige\n")
+    profesor.corregirExamenes()
 }
 
 class Profesor(val nombre: String, val listaAlumnos : MutableList<Alumno>, val listaExamen: MutableList<Examen>){
+    //el profesor asigna a cada examen el nombre de cada alumno
     fun repartirExamen(): String{
-        listaAlumnos.forEach { alumno ->
-            listaExamen.forEach { examen ->
-                examen.nombre = alumno.nombre
-            }
-        }
-        return "ha repartido el examen a cada alumno"
+        var i = 0
+        listaExamen.forEach { examen -> examen.nombre = listaAlumnos[i++].nombre }
+        return "\n$nombre ha repartido el examen a todos los alumnos\n"
     }
 
+    //a cada examen.nota le asigno un numero aleatorio y muestro el examen.nombre y el examen.nota
+    fun corregirExamenes(){
+        listaExamen.forEach { examen ->
+            examen.nota = String.format("%.2f", Random.nextDouble(0.0, 10.01)).replace(',','.').toDouble()
+            println("El alumno ${examen.nombre} ha sacado un ${examen.nota}")
+        }
+    }
 }
-class Alumno(val nombre : String, val examen : Examen){
+
+class Alumno(var nombre : String, var examen : Examen){
     fun llegar() : String{ return " he llegado"}
     fun acabadoExamen() : String{ return "El alumno $nombre ha terminado el examen"}
 }
-class Examen(var nombre : String) {
-    fun rellenarExamen() : Int{ return ((1..4).random())}
-}
+
+//rellenarExamen es para el tiempo que tarda en rellenarlo
+class Examen(var nombre: String, var nota: Double) { fun rellenarExamen() : Int{ return ((1..4).random())} }
